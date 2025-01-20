68a206b68f46e660cc28379fafffced2675ac0cf
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-01-20 03:52:12 +0000

    Add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="grid font-[family-name:var(--font-geist-sans)]">
       <Header />
 
-      <main></main>
+      <main>
+        <Hero />
+      </main>
 
       <Footer />
     </div>
