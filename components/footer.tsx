fb66896a8317327fdfab26a452287b314b0773fa
DarrickFauvel <darrick@darrickdevelops.com>
2025-06-20 10:52:57 +0000

    Make date current year

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,8 @@
 export default function Footer() {
+    const currentYear = new Date().getFullYear()
+
     return <footer className="grid text-center text-xs p-9 bg-neutral-800">
         <span className="font-bold text-gray-200">My Learning Journal</span>
-        <span className=" text-zinc-300">Copyright &copy; 2022</span>
+        <span className=" text-zinc-300">Copyright &copy; {currentYear}</span>
     </footer>
 }
