760f02ef9e76ba9ece1b96899dce0ddb78cb9c3a
DarrickFauvel <darrick@darrickdevelops.com>
2025-06-20 10:53:46 +0000

    Move posts render to variable

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import PostCard from "@/components/post-card"
 import { posts } from "@/data/posts"
 
 export default function Home() {
+const renderPosts = posts.map((post) => (
+  <PostCard {...post} key={post.id} />
+))
+
   return (
     <div className="grid font-[family-name:var(--font-source-sans-3)]">
 
       <main>
-        {posts.map((post) => (
-          <PostCard {...post} key={post.id} />
-        ))}
+        {renderPosts}
       </main>
 
     </div>
