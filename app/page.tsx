c0dbfecaa03c3995f90c5ec03ec4bbff4a167efc
DarrickFauvel <darrick@darrickdevelops.com>
2025-06-20 10:55:22 +0000

    Indent variable

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import PostCard from "@/components/post-card"
 import { posts } from "@/data/posts"
 
 export default function Home() {
-const renderPosts = posts.map((post) => (
-  <PostCard {...post} key={post.id} />
-))
+  const renderPosts = posts.map((post) => (
+    <PostCard {...post} key={post.id} />
+  ))
 
   return (
     <div className="grid font-[family-name:var(--font-source-sans-3)]">
